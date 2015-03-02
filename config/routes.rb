8c898797bc2919d5c8abecc43b7721a350145912
Derek Carlson <derekcarlson@hotmail.com>
2015-03-01 19:22:31 -0600

    generate profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  get 'static_pages/home'
 
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: :register

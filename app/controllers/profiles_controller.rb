0ecc55e4bca69745ed68c4970274cd4a2c2d0053
Derek Carlson <derekcarlson@hotmail.com>
2015-03-02 06:59:42 -0600

    generate profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,24 @@
 class ProfilesController < ApplicationController
+
+	before_filter :require_login
+
   def show
+  	@user = User.find_by_id(params[:id])
+  	if @user
+  		@startups = @user.startups.all
+  		render action: :show
+  	else
+  		render file: 'public/404', status: 404, formats: [:html]
+  	end
   end
+
+private
+
+  def require_login
+    unless current_user
+    flash[:alert] = "You must create an account or log in to view profile pages"
+      redirect_to login_url
+    end
+  end
+
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,8 @@
 class Startup < ActiveRecord::Base
 	belongs_to :user
+	validates :name, presence: true
+	validates :pitch, presence: true
+	validates :position, presence: true
+	validates :equity, presence: true
+	validates :stage, presence: true
 end
